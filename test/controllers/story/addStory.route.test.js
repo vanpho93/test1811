b681f6c659639d2cc10e25d3e4036b8a84705c63
Van Pho Nguyen <vanpho02@gmail.com>
2017-12-27 09:05:35 +0700

    abcd

--- test/controllers/story/addStory.route.test.js
+++ test/controllers/story/addStory.route.test.js
@@ -1,9 +1,19 @@
 const assert = require('assert');
 const request = require('supertest');
 const Story = require('../../../src/models/Story');
+const app = require('../../../src/app');
 
 describe('Test POST /story', () => {
     it('Can add new story by POST', async () => {
-        
+        const response = await request(app)
+        .post('/story')
+        .type('form')
+        .send({ content: 'Javascript', title: 'JS' });
+        assert.equal(response.status, 200);
+        assert.equal(response.body.success, true);
+        assert.equal(response.body.story.title, 'JS');
+        const story = await Story.findOne({});
+        assert.equal(story.title, 'JS');
+        assert.equal(story.content, 'Javascript');
     });
 });
