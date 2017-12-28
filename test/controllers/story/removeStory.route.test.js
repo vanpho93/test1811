a0639e906340c1d36658252d56d4496b68616b58
Van Pho Nguyen <vanpho02@gmail.com>
2017-12-28 21:02:54 +0700

    xong

--- test/controllers/story/removeStory.route.test.js
+++ test/controllers/story/removeStory.route.test.js
@@ -4,18 +4,23 @@ const Story = require('../../../src/models/Story');
 const app = require('../../../src/app');
 
 describe.only('Test DELETE /story:id', () => {
-    it('Can remove story with id', async () => {
+    let _id;
+    beforeEach('Create 2 stories for test', async () => {
         await Story.addStory('PHP', 'java');
-        const { _id } = await Story.addStory('JS', 'javascript');
+        _id = (await Story.addStory('JS', 'javascript'))._id;
+    });
+
+    it('Can remove story with id', async () => {
         const { status, body } = await request(app).delete(`/story/${_id}`);
         assert.equal(status, 200);
         assert.equal(body.success, true);
         assert.equal(body.story.title, 'JS');
+        const stories = await Story.find({});
+        assert.equal(stories[0].title, 'PHP');
+        assert.equal(stories.length, 1);
     });
 
     it('Cannot remove story with id twice', async () => {
-        await Story.addStory('PHP', 'java');
-        const { _id } = await Story.addStory('JS', 'javascript');
         const { status, body } = await request(app).delete(`/story/${_id}`);
         assert.equal(status, 200);
         assert.equal(body.success, true);
@@ -23,6 +28,8 @@ describe.only('Test DELETE /story:id', () => {
         const response = await request(app).delete(`/story/${_id}`);
         assert.equal(response.status, 404);
         assert.equal(response.body.success, false);
-        
+        const stories = await Story.find({});
+        assert.equal(stories[0].title, 'PHP');
+        assert.equal(stories.length, 1);
     });
 });
