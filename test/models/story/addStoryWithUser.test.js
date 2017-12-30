c00a6c08ba121e4986cc19f04a9f13fa9631017b
Van Pho Nguyen <vanpho02@gmail.com>
2017-12-30 20:10:21 +0700

    populate

--- test/models/story/addStoryWithUser.test.js
+++ test/models/story/addStoryWithUser.test.js
@@ -2,6 +2,18 @@ const assert = require('assert');
 const Story = require('../../../src/models/Story');
 const User = require('../../../src/models/User');
 
-describe('Test add new story with user', () => {
-    
+describe.only('Test add new story with user', () => {
+    let _id;
+    beforeEach('Create a user for test', async () => {
+        const user = new User({ email: 'pho@gmail.com', name: 'Pho' });
+        _id = user._id;
+        await user.save();
+    });
+    it('Can add story for a user', async () => {
+        const story = new Story({ title: 'JS', content: 'Javascript', author: _id });
+        await story.save();
+        const story2 = await Story.findOne({}).populate('author');
+        assert.equal(story2.author.name, 'Pho');
+        assert.equal(story2.author.email, 'pho@gmail.com');
+    });  
 });
