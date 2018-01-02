92c5851f30a4a097d86e29ed732871332a4a930b
Van Pho Nguyen <vanpho02@gmail.com>
2018-01-02 21:21:49 +0700

    jwt

--- src/server.js
+++ src/server.js
@@ -1,23 +1,25 @@
 const express = require('express');
-const session = require('express-session');
+const cookieParser = require('cookie-parser');
+const { sign, verify } = require('./lib/jwt');
 
 const app = express();
-
-app.use(session({
-    secret: 'akjf183382473',
-    resave: false,
-    saveUninitialized: true,
-    cookie: { maxAge: 20000 }
-}));
+app.use(cookieParser());
 
 app.get('/muave', (req, res) => {
-    req.session.daMuaVe = true;
-    res.send('Da mua ve');
+    sign({ daMuaVe: true })
+    .then(token => {
+        res.cookie('xtoken', token).send('Da mua ve');
+    });
 });
 
 app.get('/vaorap', (req, res) => {
-    if (req.session.daMuaVe) return res.send('Moi xem phim');
-    res.send('Ban phai mua ve');
+    const { xtoken } = req.cookies;
+    verify(xtoken)
+    .then(obj => {
+        if (obj.daMuaVe) return res.send('Moi xem phim');
+        res.send('Ban phai mua ve');
+    })
+    .catch(err => res.send('Ban phai mua ve'));
 });
 
 app.listen(3000, () => console.log('Server started!'));
