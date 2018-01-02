39203f7103ea275808b0e686c365a5548a3cef08
Van Pho Nguyen <vanpho02@gmail.com>
2018-01-02 20:00:40 +0700

    cookies

--- src/server.js
+++ src/server.js
@@ -3,14 +3,14 @@ const cookieParser = require('cookie-parser');
 
 const app = express();
 
-app.use(cookieParser());
-app.get('/', (req, res) => res.render('home'));
+app.use(cookieParser()); 
 
 app.get('/muave', (req, res) => {
     res.cookie('DA_MUA_VE', 1);
     daMuaVe = true;
     res.send('Da mua ve');
 });
+
 app.get('/vaorap', (req, res) => {
     if (req.cookies.DA_MUA_VE) return res.send('Moi xem phim');
     res.send('Ban phai mua ve');
