f6da6d0dcd9e7a6875dca9428129ad36c0714a86
Van Pho Nguyen <vanpho02@gmail.com>
2018-01-04 20:42:56 +0700

    add token

--- src/controllers/user.route.js
+++ src/controllers/user.route.js
@@ -7,20 +7,16 @@ const userRoute = express.Router();
 
 userRoute.post('/signup', parser, (req, res) => {
     const { email, password, name } = req.body;
-    hash(password, 8)
-    .then(encrypted => {
-        const user = new User({ email, password: encrypted, name });
-        return user.save();
-    })
-    .then(u => {
-        const u2 = u.toObject();
-        delete u2.password;
-        res.send({ success: true, user: u2 });
-    })
+    User.signUp(email, password, name)
+    .then(user => res.send({ success: true, user }))
     .catch(error => res.send({ success: false, message: error.message }));
 });
 
 userRoute.post('/signin', parser, (req, res) => {
+    const { email, password } = req.body;
+    User.signIn(email, password)
+    .then(user => res.send({ success: true, user }))
+    .catch(error => res.send({ success: false, message: error.message }));
 });
 
 module.exports = userRoute;
--- src/models/User.js
+++ src/models/User.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const { hash, compare } = require('bcrypt');
+const { sign } = require('../lib/jwt');
 
 const userSchema = new mongoose.Schema({
     email: { type: String, required: true, trim: true, unique: true },
@@ -8,6 +10,26 @@ const userSchema = new mongoose.Schema({
 });
 
 const UserModel = mongoose.model('User', userSchema);
+class User extends UserModel {
+    static async signUp(email, password, name) {
+        const encrypted = await hash(password, 8);
+        const user = new User({ email, password: encrypted, name });
+        await user.save();
+        const u = user.toObject();
+        delete u.password;
+        return u;
+    }
 
-class User extends UserModel {}
+    static async signIn(email, password) {
+        const user = await User.findOne({ email });
+        if (!user) throw new Error('Cannot find user');
+        const same = await compare(password, user.password);
+        if (!same) throw new Error('Password is incorrect');
+        const u = user.toObject();
+        delete u.password;
+        const token = await sign({ _id: u.id });
+        u.token = token;
+        return u;
+    }
+}
 module.exports = User;
