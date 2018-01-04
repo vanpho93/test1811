bdce70e4c18292e226b097342913b1b2ea054908
Van Pho Nguyen <vanpho02@gmail.com>
2018-01-04 21:19:55 +0700

    xong

--- src/controllers/story.route.js
+++ src/controllers/story.route.js
@@ -32,16 +32,16 @@ router.post('/', parser, (req, res) => {
 });
 
 router.delete('/:id', (req, res) => {
-    // verify(token)
-    // .then(obj => Story.findByIdAndRemove(req.params.id))
-    // .then(story => {
-    //     if (!story) return res.status(404).send({
-    //         success: false,
-    //         message: 'Cannot find story'
-    //     });
-    //     res.send({ success: true, story });
-    // })
-    // .catch(err => res.status(400).send({ success: false, error: error.message }))
+    verify(req.headers.token)
+    .then(obj => Story.findByIdAndRemove(req.params.id))
+    .then(story => {
+        if (!story) return res.status(404).send({
+            success: false,
+            message: 'Cannot find story'
+        });
+        res.send({ success: true, story });
+    })
+    .catch(error => res.status(400).send({ success: false, error: error.message }))
 });
 
 module.exports = router;
